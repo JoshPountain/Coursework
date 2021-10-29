44b6f8f77fc0f3652522b2a31b67c2895fb2aba8
Your Name <joshpountain@yahoo.co.uk>
2021-10-29 03:40:43 +0100

    Working facing formulas(no zreo handling)

--- Coursework/Graphics/Camera.cs
+++ Coursework/Graphics/Camera.cs
@@ -13,19 +13,30 @@ namespace Graphics
             fov[0] = 10;
             fov[0] = 10;
 
-            /*location[0] = renderDist / 2;
+
+            location[0] = renderDist / 2;
+
+            location[0] = renderDist / 2;
+
             location[1] = location[0];
             location[2] = location[0];
             facing[0] = location[0];
             facing[1] = location[1];
-            facing[2] = location[2] + 1;*/
+
+            facing[2] = location[2] + 1;
         }
         public int[] location { get; set; } = new int[3] { 0, 0, 0 };
-        public double[] facing { get; set;} = new double[3] { 0, 1, 0 };
+        public double[] facing { get; set;} = new double[3] { 0, 0, 1 };
         public double[] rotation { get; set; } = new double[2];
         public double[] fov { get; set; } = new double[2] { 0, 0 };
 
         
+
+
+        //facing[2] = location[2] + 1;
+        
+        
+
 
         public void checkDebug()
         {
@@ -45,115 +56,79 @@ namespace Graphics
             {
                 Console.WriteLine("FOV: " + i.ToString()+ " " + fov[i].ToString());
             }
+
             foreach (int i in Enumerable.Range(0, 3))
             {
                 Console.WriteLine("facing:" + i.ToString() + " " + facing[i].ToString());
             }
         }
-        public void rotate(double horizontal, double vertical)
+        
+
+        public void rotate(double Atheta, double Av)
         {
-            //trig to find new facing point
+            //trig angles to find new facing point
+            rotation[0] += Atheta;
+            rotation[1] += Av;
+            //Cam position modifiers (Asif top down)
+            double x = 0;
+            double y = 0;
+            double z = 0;
+            //t is for x,y rotation, a is for the 'elevation'
+            double t = rotation[0];
+            double a = rotation[1];
+            double modifier = 90;
+            //Trig functions in c# use radeons, the convert value will make the output be in degrees
+            double CONVERT = Math.PI / 180;
+            double sin;
+            double cos;
+            double proportion;
+            if (t >= 360)
+            {
+                t -= 360;
+            }
 
-            //Camera works by having a point it is 'looking' which is centeral to the camera view
-            /*Mouse movements can move vertically and horizontally, to move where camera is looking, equations can be used to move the point
-             * equally, keeping the point equal distance away; the equations are:
-             * x = x + Cos(up)*(x+2*(Sin(side/2))*(Cos(side/2)))
-             * y = y + Cos(up)*(y+(1-2*(Sin(Side/2))^2)
-             * z = z - Sin(up)
-             * These work for any [-360<= horizontal movement <= 360] [-360 <= vertical movement <= 360]
-            */
-
-
-            //x, y, z mixup
-            double side = horizontal;
-            double up = vertical;
-            double hUp = up / 2;
-            double hSide = side / 2;
-            double x = facing[0];
-            double y = facing[1];
-            double z = facing[2];
-            double k = 0;
-
-
-            double a = (side / 2) * (Math.PI / 180);
-            double b = 1 - Math.Sqrt(Math.Pow((2 * Math.Sin(a)), 2) - Math.Pow(((2 * Math.Sin(a)) * (Math.Sin(a))), 2));
-
-
-
-            Console.WriteLine("UP val :" + up.ToString() + "  Side Val : " + side.ToString());
-            Console.WriteLine("half UP val :" + hUp.ToString() + " Half Side Val : " + hSide.ToString());
-            up = up * (Math.PI / 180);
-            //side = side * (Math.PI / 180);
-            hUp = hUp * (Math.PI / 180);
-            hSide = hSide * (Math.PI / 180);
-            Console.WriteLine("UP val :" + up.ToString() + "  Side Val : " + side.ToString());
-            Console.WriteLine("half UP val :" + hUp.ToString() + " Half Side Val : " + hSide.ToString());
             while (true)
             {
-                if(side <= 360)
+                if (t <= modifier)
                 {
                     break;
                 }
                 else
                 {
-                    side -= 360;
+                    modifier += 90;
                 }
             }
+            
+            z = (Av / Math.Abs(Av)) * Math.Sin(Av * CONVERT);
+            proportion = Math.Cos(Av * CONVERT);
 
-            if (side <= 90)
+            sin = (Math.Sin((modifier - Atheta) * CONVERT)) * proportion;
+            cos = (Math.Cos((modifier - Atheta) * CONVERT)) * proportion;
+
+            switch (modifier)
             {
-                k = 90 - side;
-            }else if(side <= 180)
-            {
-                k = 180 - side;
-            }else if(side <= 270)
-            {
-                k = 270 - side;
+                case 90:
+                    y += sin;
+                    x += cos;
+                    break;
+                case 180:
+                    x += sin;
+                    y -= cos;
+                    break;
+                case 270:
+                    y -= sin;
+                    x -= cos;
+                    break;
+                case 360:
+                    x -= sin;
+                    y += cos;  
+                    break;
             }
-            else
-            {
-                k = 360 - side;
-            }
 
-            //x = (x + (2 * (Math.Sin(horizontal / 2)) * (Math.Cos(horizontal / 2)))) - ((1 - Math.Cos(k))*(Math.Sin(k)));
-            //z = (z + (1 - 2*Math.Pow(Math.Sin(horizontal / 2), 2))) - (1-Math.Cos(k)*(Math.Cos(k)));
-            //y = y - Math.Sin(up);
+            facing[0] += x;
+            facing[1] += y;
+            facing[2] += z;
 
-            //Stage 1 equations
-
-            x = 2 * Math.Sin(hSide) * Math.Cos(hSide);
-
-            //y = 1 - 2 * Math.Pow(Math.Sin(side / 2), 2);
-            //y = 1 - (2 * Math.Sin(90/2 * Math.PI / 180)) * Math.Sin(90 / 2 * Math.PI / 180);
-
-            Console.WriteLine("x : " + x.ToString());
-            Console.WriteLine("y : " + b.ToString());
-
-            Console.WriteLine(Math.Sin(90 / 2 * Math.PI / 180).ToString());
-
-            /*
-             
-            Console.WriteLine("x :" + x.ToString());
-            x = x + Math.Cos(up) * (x + (2 * Math.Sin(side / 2) * Math.Cos(side / 2)));
-            //x = Math.Cos(up) * (x + (2 * Math.Sin(side / 2) * Math.Cos(side / 2)));
-            Console.WriteLine("New x: " + x.ToString());
-
-
-            Console.WriteLine("z :" + z.ToString());
-            //double temp = (z + (1 - (2 * Math.Pow(Math.Sin(side / 2), 2))));
-            double temp = (1 - (2 * Math.Pow(Math.Sin(side / 2), 2)));
-
-            double temp2 = 0("New z: " + z.ToString());
-
-
-            Console.WriteLine("y :" + y.ToString());
-            y = y - Math.Sin(up);
-            Console.WriteLine("New y: " + y.ToString());
-            */
-
-            facing[0] = x;
-            facing[1] = y;
-            facing[2] = z;
         }
         public void Wmove(double amplifier)
         {
--- Coursework/Graphics/Form1.cs
+++ Coursework/Graphics/Form1.cs
@@ -32,39 +32,9 @@ namespace Graphics
             //trig right angle from camera to centre will give furthest side point cam can see
             //need formula for working out which way the camera is looking.
             //Have point the camera is looking at!!!
-            //camera always positioned in middle of render area!!!!
-            int renderDist = 100;
-            renderDist *= 2;
-            int[,,] space = new int[renderDist, renderDist, renderDist];
+            
+            
 
-            int[,,] render = space;
-            //x, y, z
-            double[] zero2 = new double[2] { 0, 0 };
-            double[] zero3 = new double[3] { 0, 0, 0 };
-            int[] pos = new int[2] { 50, 50 };
-            int i = 0;
-            var test = new Objects[100];
-            var cam = new Camera(100);
-
-            //cam.checkDebug();
-            //cam.rotate(90, 0);
-            //cam.checkDebug();
-
-            //test[0] = new Objects(10, pos, 2);
-            //go through all objects
-            //equation to tell which objects can be seen
-            /*foreach (object t in test)
-            {
-                i++;
-            }
-            Console.WriteLine(i.ToString());
-            Objects o = test[0];
-            o.tryMe();*/
-
-            //double h = o.radius;
-            //cam.checkDebug();
-            cam.rotate(180, 0);
-            //cam.checkDebug();
         }
     }
 }
